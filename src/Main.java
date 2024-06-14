95aac2a01c303dcaff7a2eda8e190fb133d4a7f3
Jovanny Ramos <91354201+ItsJeff510@users.noreply.github.com>
2024-06-13 18:21:49 -0700

    Updating small bugs

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
     }
     public static void runMenu() {
         int input = 0;
+        int counter = 0;
+        boolean trigger = false;
         HashMap<String, Double> people = new HashMap<>();
         HashMap<String, Double> amountToRecieve = new HashMap<>();
         HashMap<String, Double> amountToSend = new HashMap<>();
@@ -15,7 +17,8 @@ public class Main {
         double money = -1.0;
         double total = 0.0;
         Scanner obj = new Scanner(System.in);
-        while (input != 6) {
+
+        while (input != 7) {
             System.out.println();
             printMenu();
 
@@ -31,63 +34,85 @@ public class Main {
                         System.out.print("Enter paid value (enter 0 when done): ");
                         money = Double.parseDouble(obj.nextLine());
                         value += money;
-                        System.out.print("Current Total: $");
+                        System.out.print("\nCurrent Total: $");
                         System.out.printf("%.2f\n", value);
                     }
                     people.put(name, value);
                     money = -1.0;
                     value = 0.0;
                 }
-                case 2 -> { // Case two takes all the people and values and displays them
+                case 2 -> {
+                    System.out.print("Enter the name of the person you want to remove: ");
+                    name = obj.nextLine();
+                    for (String i : people.keySet()) {
+                        if (i.equals(name)) {
+                            people.remove(name);
+                            System.out.printf("%s has been removed.", name);
+                            trigger = true;
+                        }
+                    }
+
+                    if (trigger == false) {
+                        System.out.println("\nThe name was not found, please try another name or be Case Sensetive.");
+                    } else {
+                        trigger = false;
+                    }
+                }
+                case 3 -> { // Case three takes all the people and values and displays them
                     for (String i : people.keySet()) {
                         System.out.printf("%s :", i);
                         System.out.printf(" $%.2f\n", people.get(i));
                     }
                 }
-                case 3 -> { // Case three takes the total and divides it evenly
+                case 4 -> { // Case four takes the total and divides it evenly
+                    total = 0.0;
+                    counter = 0;
                     for (Double i : people.values()) {
                         total += i;
-
-                        System.out.print("\nThe total is: $");
-                        System.out.printf("%.3f", total);
+                        counter++;
+                        System.out.print("\nThe total is after person ");
+                        System.out.printf("%s: $%.2f", counter, total);
                     }
                     total /= people.size();
                     System.out.println();
                 }
-                case 4 -> { // Case 4 Takes care of giving everyone's proper pay value
-                    if(total != 0) {
+                case 5 -> { // Case five Takes care of giving everyone's proper pay value
+                    if (!amountToSend.isEmpty() && !amountToRecieve.isEmpty()) {
+                        amountToSend.clear();
+                        amountToRecieve.clear();
+                    }
+
+                    if (total != 0) {
                         for (String i : people.keySet()) {
-                            System.out.printf("\n%s", i);
 
                             if (people.get(i) - total < 0) {
-                                System.out.print(" owes ");
-                                System.out.printf("%.2f", Math.abs(people.get(i) - total));
                                 amountToSend.put(i, Math.abs(people.get(i) - total));
                             } else {
-                                System.out.print(" is owed ");
-                                System.out.printf("%.2f", Math.abs(people.get(i) - total));
                                 amountToRecieve.put(i, Math.abs(people.get(i) - total));
                             }
                         }
+                        System.out.println("Owe/Owed has been calculated! Run option 6 to see the amounts owe/owed.");
                     } else {
                         System.out.println("This method cannot be done, try dividing equally first.");
                     }
                     total = 0.0;
+                }
+                case 6 -> { // Case six is meant to show who owes what to who
+
+                    if (!amountToSend.isEmpty() && !amountToRecieve.isEmpty()) {
+                        for (String i : amountToSend.keySet()) {
+                            System.out.printf("\n%s owes %.2f", i, amountToSend.get(i) / amountToRecieve.size());
+                        }
+                        for (String i : amountToRecieve.keySet()) {
+                            System.out.printf("\n%s is owed %.2f", i, amountToRecieve.get(i) / amountToSend.size());
+                        }
+                    } else {
+                        System.out.println("This method cannot be done, try showing who is owed first or adding more than 1 person.");
+                    }
                     System.out.println();
                 }
-                case 5 -> { // Case 5 is meant to show who owes what to who
-                    for (String i : amountToSend.keySet()) {
-                        System.out.printf("\n%s owes %.2f", i, amountToSend.get(i) / amountToRecieve.size());
-                    }
-                    for (String i : amountToRecieve.keySet()) {
-                        System.out.printf("\n%s is owed %.2f", i, amountToRecieve.get(i) / amountToSend.size());
-                    }
-
-                    amountToSend.clear();
-                    amountToRecieve.clear();
-                }
-                case 6 -> { // Is an end statement meant to stop the While-loop
-                    System.out.println("Thank you for using, Exiting!");
+                case 7 -> { // Is an end statement meant to stop the While-loop
+                    System.out.println("Thank you for using, Exiting!\n");
                 }
 
 
@@ -97,34 +122,16 @@ public class Main {
     }
 
     public static void printMenu() {
-        System.out.printf("\n*****************************" +
+        System.out.printf("*****************************" +
                           "\n*            Menu           *" +
                           "\n*                           *" +
                           "\n* 1) Add new person         *" +
-                          "\n* 2) List all people/values *" +
-                          "\n* 3) Divide Equally         *" +
-                          "\n* 4) Show who is owed       *" +
-                          "\n* 5) Show who owes who      *" +
-                          "\n* 6) Exit                   *" +
+                          "\n* 2) Remove person          *" +
+                          "\n* 3) List all people/values *" +
+                          "\n* 4) Divide Equally         *" +
+                          "\n* 5) Calculate Owe/Owed     *" +
+                          "\n* 6) Show Owe/Owed          *" +
+                          "\n* 7) Exit                   *" +
                           "\n*****************************\n");
     }
 }
-
-// Calculator divider project
-/*
-1) need a method to store all values based on who paid
-    This method has been done utilizing a Hashmap to store both the name
-    paired with value of amount paid
-
-2) need to divide all numbers equally between people
-    One method to do this can be by equally dividing everyones paid amount
-    into equal amounts and pair them by such but gives off a huge complicated
-    code. Another way to tackle this could be to get the total amount needed
-    to be paid and subtracting it to find those who paid most. Dividing their
-    amount by those who still owe and distribute as such.
-
-3) need a method to update any new values given
-
-Starting with just creating a method to store both a name and their value
-
-*/
